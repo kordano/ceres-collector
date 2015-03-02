d3878f45a3623a02feeeee2e090a2f336bbdd39e
kordano <konrad.kuehne@rocketmail.com>
2015-03-02 15:37:42 +0100

    bump importer

--- opt/import-it.sh
+++ opt/import-it.sh
@@ -2,10 +2,10 @@
 # Import yesterday's collected data, data path as argument
 yesterday=$(date -d yesterday +'%Y-%m-%d')
 data=$1
-mongoimport --db demeter --collection tweets $data/tweets-$yesterday.json
-mongoimport --db demeter --collection refs $data/refs-$yesterday.json
-mongoimport --db demeter --collection tags $data/tags-$yesterday.json
-mongoimport --db demeter --collection users $data/users-$yesterday.json
-mongoimport --db demeter --collection urls $data/urls-$yesterday.json
-mongoimport --db demeter --collection htmls $data/htmls-$yesterday.json
-mongoimport --db demeter --collection messages $data/messages-$yesterday.json
+mongoimport --db hera --collection tweets $data/tweets-$yesterday.json
+mongoimport --db hera --collection refs $data/refs-$yesterday.json
+mongoimport --db hera --collection tags $data/tags-$yesterday.json
+mongoimport --db hera --collection users $data/users-$yesterday.json
+mongoimport --db hera --collection urls $data/urls-$yesterday.json
+mongoimport --db hera --collection htmls $data/htmls-$yesterday.json
+mongoimport --db hera --collection messages $data/messages-$yesterday.json
