314b96d829b2342c2ff4202385a57b274136430b
kordano <konrad.kuehne@rocketmail.com>
2015-03-10 10:59:16 +0100

    adjust import script

--- opt/import-date.sh
+++ opt/import-date.sh
@@ -7,3 +7,12 @@ mongoimport --db juno --collection users users-$d.json
 mongoimport --db juno --collection urls urls-$d.json
 mongoimport --db juno --collection htmls htmls-$d.json
 mongoimport --db juno --collection messages messages-$d.json
+mongoimport --db juno --collection mentions refs-$d.json
+mongoimport --db juno --collection sources refs-$d.json
+mongoimport --db juno --collection replies refs-$d.json
+mongoimport --db juno --collection retweets refs-$d.json
+mongoimport --db juno --collection shares refs-$d.json
+mongoimport --db juno --collection pubs refs-$d.json
+mongoimport --db juno --collection urlrefs refs-$d.json
+mongoimport --db juno --collection tagrefs refs-$d.json
+mongoimport --db juno --collection unknown refs-$d.json
