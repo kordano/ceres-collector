c9626bb833211ad4728ae945e422ca0d31d728db
kordano <konrad.kuehne@rocketmail.com>
2015-04-01 10:53:59 +0200

    update import scripts

--- opt/fetch-date.sh
+++ opt/fetch-date.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+t=$2
 d=$1
-scp -P 2023 konrad@mono.topiq.es:/home/konrad/data/ceres-collector/*-$d.json ./
+scp -P 2023 konrad@mono.topiq.es:/home/konrad/data/ceres-collector/*-$d.json $t
--- opt/import-date.sh
+++ opt/import-date.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+t=$2
 d=$1
-mongoimport --db juno --collection tweets tweets-$d.json
-mongoimport --db juno --collection refs refs-$d.json
-mongoimport --db juno --collection tags tags-$d.json
-mongoimport --db juno --collection users users-$d.json
-mongoimport --db juno --collection urls urls-$d.json
-mongoimport --db juno --collection htmls htmls-$d.json
-mongoimport --db juno --collection messages messages-$d.json
-mongoimport --db juno --collection mentions refs-$d.json
-mongoimport --db juno --collection sources refs-$d.json
-mongoimport --db juno --collection replies refs-$d.json
-mongoimport --db juno --collection retweets refs-$d.json
-mongoimport --db juno --collection shares refs-$d.json
-mongoimport --db juno --collection pubs refs-$d.json
-mongoimport --db juno --collection urlrefs refs-$d.json
-mongoimport --db juno --collection tagrefs refs-$d.json
-mongoimport --db juno --collection unknown refs-$d.json
+mongoimport --db juno --collection tweets $t/tweets-$d.json
+mongoimport --db juno --collection tags $t/tags-$d.json
+mongoimport --db juno --collection users $t/users-$d.json
+mongoimport --db juno --collection urls $t/urls-$d.json
+mongoimport --db juno --collection htmls $t/htmls-$d.json
+mongoimport --db juno --collection messages $t/messages-$d.json
+mongoimport --db juno --collection mentions $t/mentions-$d.json
+mongoimport --db juno --collection sources $t/sources-$d.json
+mongoimport --db juno --collection replies $t/replies-$d.json
+mongoimport --db juno --collection retweets $t/retweets-$d.json
+mongoimport --db juno --collection shares $t/shares-$d.json
+mongoimport --db juno --collection pubs $t/pubs-$d.json
+mongoimport --db juno --collection urlrefs $t/urlrefs-$d.json
+mongoimport --db juno --collection tagrefs $t/tagrefs-$d.json
+mongoimport --db juno --collection unknown $t/unknown-$d.json
